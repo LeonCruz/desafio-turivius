4b3235911b3bc772d7aed5daeda894f3ec066e2e
Leon Cruz <leon.cruz.teixeira@gmail.com>
2021-02-09 17:41:40 -0300

    Modelo do banco de dados para os livros

--- toscrape/books/models.py
+++ toscrape/books/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=250)
+    price = models.FloatField()
+    description = models.TextField()
+    url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
