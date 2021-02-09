ce2c02aa5eac420d53ca6092ff4fa429968269f6
Leon Cruz <leon.cruz.teixeira@gmail.com>
2021-02-09 19:03:45 -0300

    Campo adicional para exibir a url do livro na api

--- toscrape/books/serializer.py
+++ toscrape/books/serializer.py
@@ -1,9 +1,17 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.reverse import reverse
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from books.models import Book
 
 
 class BookSerializer(ModelSerializer):
+    url_api = SerializerMethodField("get_url_api")
+
     class Meta:
         model = Book
-        fields = "__all__"
+        fields = ["id", "title", "price", "description", "url", "url_api"]
+
+    def get_url_api(self, instance):
+        """Campo adicional para exibir o url do livro na api."""
+        request = self.context.get("request")
+        return reverse("book-detail", args=[instance.id], request=request)
