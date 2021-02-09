3bb1c8269f92df9f5e84a4095524c90fbec6031a
Leon Cruz <leon.cruz.teixeira@gmail.com>
2021-02-09 19:41:01 -0300

    Função que salva os dados dos livros no banco de dados

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -4,7 +4,7 @@ from urllib.parse import urljoin
 
 from bs4 import BeautifulSoup
 from httpx import AsyncClient
-from requests import get
+from requests import get, post
 
 URL_BASE = "http://books.toscrape.com/catalogue/"
 CATEGORY = "category/books/sequential-art_5/"
@@ -14,6 +14,7 @@ URL = urljoin(URL_BASE, CATEGORY)
 
 def get_urls(page=None):
     """Retorna uma lista com os links dos livros da página."""
+    print("Pegandos urls dos livros")
     if page is not None:
         url = urljoin(URL, page)
         html = get(url).content
@@ -24,7 +25,7 @@ def get_urls(page=None):
 
     links = []
     for a in soup.select("h3 > a"):
-        links.append(a["href"])
+        links.append(a)
 
     try:
         next_page = soup.select_one("li.next > a")["href"]
@@ -34,9 +35,10 @@ def get_urls(page=None):
     return links, next_page
 
 
-async def get_book_information(book_url):
+async def get_book_information(book):
     """Retorna as informações de cada livro."""
-    url = urljoin(URL_BASE, urljoin("catalogue/", book_url))
+    print(f"Pegando informações do livro {book['title']}")
+    url = urljoin(URL_BASE, urljoin("catalogue/", book["href"]))
 
     async with AsyncClient() as client:
         html = await client.get(url)
@@ -44,11 +46,18 @@ async def get_book_information(book_url):
     soup = BeautifulSoup(html, "html.parser")
 
     title = soup.find("h1").text
-    price = soup.find("p", class_="price_color").text
+    price = soup.find("p", class_="price_color").text.removeprefix("£")
     tag_description = soup.find("div", id="product_description")
     description = tag_description.find_next_sibling("p").text
 
     return dict(title=title, price=price, description=description, url=url)
+
+
+def save_book(book):
+    api_url = "http://localhost:8000/books/"
+    response = post(api_url, data=book)
+    if response.status_code == 201:
+        print(f"O livro {book['title']} foi salvo com sucesso")
 
 
 async def main():
@@ -65,7 +74,10 @@ async def main():
 
         links, next_page = get_urls(next_page)
 
-    print(len(books))
+    for book in books:
+        save_book(book)
+
+    print(f"Total de livros salvos: {len(books)}")
 
 
 run(main())
