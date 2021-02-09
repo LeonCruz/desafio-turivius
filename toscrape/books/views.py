a493a80a8c5d71d8df528c901c2d7c84ebbd4d25
Leon Cruz <leon.cruz.teixeira@gmail.com>
2021-02-09 17:43:30 -0300

    Definição da viewset

--- toscrape/books/views.py
+++ toscrape/books/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from books.models import Book
+from books.serializer import BookSerializer
+
 
 # Create your views here.
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
